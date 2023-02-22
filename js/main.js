81c01f8ef12862c5bf5e8e2eef71fab7576d0051
leonardoserra <serra.leonardo92@gmail.com>
2023-02-22 15:30:38 +0100

    fix delle formule percentuali, if interno

--- js/main.js
+++ js/main.js
@@ -1,25 +1,30 @@
 const age = parseInt(prompt("Buongiorno, inserire età"));
 const distance = parseInt(prompt("Inserire distanza che si vuole percorrere in km"));
 const pricePerKm = 0.21;
-let price;
-console.log(age, distance, pricePerKm);
+let price = distance * pricePerKm;
+
+console.log(age, distance, pricePerKm, price);
+
 
 if(isNaN(age)||isNaN(distance)){
+
     document.getElementById("messaggio").innerHTML = "errore, inserire solo numeri interi"
-} 
 
-price = distance * pricePerKm;
-console.log(price)
+} else{ 
 
-if(age < 18){
-    price = ((price*100)/20) - price;
-    document.getElementById("costo").innerHTML = `Buongiorno, il biglietto costa ${price}€ perchè sei under18 e hai uno sconto del 20%. Buon Viaggio!`
+    if(age < 18){
+        price = price - ((price*20)/100);
+        document.getElementById("costo").innerHTML = `Buongiorno, il biglietto costa ${price}€ perchè sei under18 e hai uno sconto del 20%. Buon Viaggio!`
 
-}else if (age >= 65){
-    price = ((price*100)/40) - price;
-    document.getElementById("costo").innerHTML = `Buongiorno, il biglietto costa ${price}€ perchè sei over65 e hai uno sconto del 40%. Buon Viaggio!`
-} else{
-    document.getElementById("costo").innerHTML = `Buongiorno, il biglietto costa ${price}€. Buon Viaggio!`
-
+    }else if (age >= 65){
+        price = price - ((price*40)/100);
+        document.getElementById("costo").innerHTML = `Buongiorno, il biglietto costa ${price}€ perchè sei over65 e hai uno sconto del 40%. Buon Viaggio!`
+    } else{
+        document.getElementById("costo").innerHTML = `Buongiorno, il biglietto costa ${price}€. Buon Viaggio!`
+    }
+    
 }
-console.log(price.toFixed(2))
+
+
+
+
