f1ae59bfaf2a4cdce7227fe18399c4086214bb82
leonardoserra <serra.leonardo92@gmail.com>
2023-02-22 15:43:34 +0100

    aggiunto " : " alla fine di ogni riga. FINE

--- js/main.js
+++ js/main.js
@@ -8,26 +8,26 @@ console.log(age, distance, pricePerKm, price);
 
 if(isNaN(age)||isNaN(distance)){
 
-    document.getElementById("messaggio").innerHTML = "errore, inserire solo numeri interi"
+    document.getElementById("messaggio").innerHTML = "errore, inserire solo numeri interi";
 
 } else{ 
 
     if(age < 18){
 
         price = price - ((price*20)/100);
-        document.getElementById("messaggio").innerHTML = `Buongiorno, sei under18 perció hai uno sconto del 20%:`
-        document.getElementById("costo").innerHTML = `- il biglietto costa ${price}€. Buon Viaggio!`
+        document.getElementById("messaggio").innerHTML = `Buongiorno, sei under18 perció hai uno sconto del 20%:`;
+        document.getElementById("costo").innerHTML = `il biglietto costa ${price}€. Buon Viaggio!`;
 
     }else if (age >= 65){
 
         price = price - ((price*40)/100);
-        document.getElementById("messaggio").innerHTML = `Buongiorno, lei ha diritto allo sconto over65 del 40%:`
-        document.getElementById("costo").innerHTML = `- il biglietto costa ${price}€. Buon Viaggio!`
+        document.getElementById("messaggio").innerHTML = `Buongiorno, lei ha diritto allo sconto over65 del 40%:`;
+        document.getElementById("costo").innerHTML = `il biglietto costa ${price}€. Buon Viaggio!`;
     
     } else{
 
-        document.getElementById("messaggio").innerHTML = `Buongiorno, il prezzo del biglietto è il seguente:`
-        document.getElementById("costo").innerHTML = `${price}€. Buon Viaggio!`
+        document.getElementById("messaggio").innerHTML = `Buongiorno, il prezzo del biglietto è il seguente:`;
+        document.getElementById("costo").innerHTML = `${price}€. Buon Viaggio!`;
 
     }
     
